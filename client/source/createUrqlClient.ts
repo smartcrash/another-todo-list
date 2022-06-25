cb96445d155bba38799cfb0b2b56da9479b2d9f2
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-25 08:30:59 -0400

    Drop unnecessary `updateQuery` function

--- client/source/createUrqlClient.ts
+++ client/source/createUrqlClient.ts
@@ -1,9 +1,6 @@
 
 import {
-  Cache,
-  cacheExchange,
-  DataFields,
-  QueryInput
+  cacheExchange
 } from "@urql/exchange-graphcache";
 import {
   createClient,
@@ -16,23 +13,8 @@ import {
   CreateUserMutation,
   CurrentUserDocument,
   CurrentUserQuery,
-  LoginWithPasswordMutation,
-  LogoutMutation
+  LoginWithPasswordMutation
 } from "./generated/graphql";
-
-function updateQuery<R extends DataFields, Q>(
-  cache: Cache,
-  queryInput: QueryInput,
-  result: any,
-  // The `data` may be null if the cache doesn't actually have
-  // enough locally cached information to fulfil the query
-  fn: (result: R, data: Q | null) => Q | null
-) {
-  return cache.updateQuery(
-    queryInput,
-    (data) => fn(result, data as any) as any
-  );
-}
 
 export const createUrqlClient = () => createClient({
   url: API_URL,
@@ -42,37 +24,25 @@ export const createUrqlClient = () => createClient({
     cacheExchange({
       updates: {
         Mutation: {
-          loginWithPassword: (result, args, cache, info) => {
-            updateQuery<LoginWithPasswordMutation, CurrentUserQuery>(
-              cache,
-              { query: CurrentUserDocument },
-              result,
-              (result, data) => {
-                if (result.loginWithPassword.errors) return data;
-                else return { currentUser: result.loginWithPassword.user };
-              }
-            );
+          loginWithPassword(result: LoginWithPasswordMutation, args, cache, info) {
+            cache.updateQuery({ query: CurrentUserDocument }, (data: CurrentUserQuery | null) => {
+              if (result.loginWithPassword.errors) return data;
+              else return { currentUser: result.loginWithPassword.user };
+            })
           },
 
-          createUser: (result, args, cache, info) => {
-            updateQuery<CreateUserMutation, CurrentUserQuery>(
-              cache,
-              { query: CurrentUserDocument },
-              result,
-              (result, data) => {
-                if (result.createUser.errors) return data;
-                else return { currentUser: result.createUser.user };
-              }
-            );
+          createUser(result: CreateUserMutation, args, cache, info) {
+            cache.updateQuery({ query: CurrentUserDocument }, (data: CurrentUserQuery | null) => {
+              if (result.createUser.errors) return data;
+              else return { currentUser: result.createUser.user };
+            })
           },
 
-          logout: (result, args, cache, info) => {
-            updateQuery<LogoutMutation, CurrentUserQuery>(
-              cache,
+          logout(result, args, cache, info) {
+            cache.updateQuery(
               { query: CurrentUserDocument },
-              result,
               () => ({ currentUser: null })
-            );
+            )
           },
         },
       },
