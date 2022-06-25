5752ef66b750762e07bfab144c74fb3fd9049722
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-25 07:03:07 -0400

    Create Project entity

---
+++ source/entity/Project.ts
@@ -0,0 +1,29 @@
+import { Field, ObjectType } from "type-graphql";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "./User";
+
+@ObjectType()
+@Entity()
+export class Project {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  title: string;
+
+  @Column()
+  userId: number
+
+  @ManyToOne(() => User, user => user.projects, { onDelete: 'CASCADE' })
+  user: User
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
