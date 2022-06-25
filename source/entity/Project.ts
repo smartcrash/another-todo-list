7785a615a27991c9a8d5d3e1169dcbd73d296186
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-25 11:24:20 -0400

    Use `slugify` instead of `_.kebabCase`

--- source/entity/Project.ts
+++ source/entity/Project.ts
@@ -1,9 +1,13 @@
-import { kebabCase } from "lodash";
+import slugify from 'slugify'
 import { Field, ObjectType } from "type-graphql";
 import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { User } from "./User";
 
-const uniqueId = () => (Math.random() + 1).toString(36).substring(7)
+/**
+ * Generates an unique ID.
+ * @see https://stackoverflow.com/a/8084248
+ */
+const uniqueId = () => (Math.random() + 1).toString(36).substring(2)
 
 @ObjectType()
 @Entity()
@@ -24,7 +28,7 @@ export class Project {
   @BeforeUpdate()
   async createSlug() {
     if (this.title) {
-      this.slug = kebabCase(this.title) + uniqueId()
+      this.slug = slugify(this.title) + '-' + uniqueId()
     }
   }
 
